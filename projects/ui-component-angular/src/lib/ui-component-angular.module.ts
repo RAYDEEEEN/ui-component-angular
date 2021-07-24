970cfd13557ea7dca28bcdaf3e647681cb00d9b4
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 21:44:00 +0200

    :bug:

--- projects/ui-component-angular/src/lib/ui-component-angular.module.ts
+++ projects/ui-component-angular/src/lib/ui-component-angular.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { NumericValueAccessor } from './directives/number-value-accessor';
 import { MyInput } from './directives/proxies';
+import { TextValueAccessor } from './directives/text-value-accessor';
+import { ValueAccessor } from './directives/value-accessor';
 
-const COMPONENTS = [MyInput];
+const COMPONENTS = [
+  MyInput,
+  NumericValueAccessor,
+  ValueAccessor,
+  TextValueAccessor
+];
 
 @NgModule({
   declarations: [...COMPONENTS],
