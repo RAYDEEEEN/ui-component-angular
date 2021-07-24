0c05ac1010a36d634e62eeb9a4cf3eea758800c8
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 21:46:03 +0200

    :bug:

--- projects/ui-component-angular/src/lib/ui-component-angular.module.ts
+++ projects/ui-component-angular/src/lib/ui-component-angular.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { NumericValueAccessor } from './directives/number-value-accessor';
 import { MyInput } from './directives/proxies';
 import { TextValueAccessor } from './directives/text-value-accessor';
-import { ValueAccessor } from './directives/value-accessor';
 
-const COMPONENTS = [
-  MyInput,
-  NumericValueAccessor,
-  ValueAccessor,
-  TextValueAccessor
-];
+const COMPONENTS = [MyInput, NumericValueAccessor, TextValueAccessor];
 
 @NgModule({
   declarations: [...COMPONENTS],
--- projects/ui-component-angular/src/public-api.ts
+++ projects/ui-component-angular/src/public-api.ts
@@ -1,5 +1,7 @@
 /*
  * Public API Surface of ui-component-angular
  */
+export * from './lib/directives/number-value-accessor';
 export * from './lib/directives/proxies';
+export * from './lib/directives/text-value-accessor';
 export * from './lib/ui-component-angular.module';
