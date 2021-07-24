3225697f3626768fa6fcc5dd81447c0386167ac0
canserkanuren <canserkan.uren@gmail.com>
2021-07-24 22:58:24 +0200

    :bug:

--- projects/ui-component-angular/src/lib/ui-component-angular.module.ts
+++ projects/ui-component-angular/src/lib/ui-component-angular.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { defineCustomElements } from '@raydeeeen/ui-component/loader';
 import { NumericValueAccessor } from './directives/number-value-accessor';
 import { UiInput } from './directives/proxies';
 import { TextValueAccessor } from './directives/text-value-accessor';
 
 const COMPONENTS = [UiInput, NumericValueAccessor, TextValueAccessor];
 
-// defineCustomElements(window);
+defineCustomElements(window);
 
 @NgModule({
   declarations: [...COMPONENTS],
